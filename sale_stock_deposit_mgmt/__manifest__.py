320740a47c12a97ae3dc09bb817e806b0dccd9a1
sergio-teruel <sergio.teruel@tecnativa.com>
2020-11-11 01:26:33 +0100

    [ADD] sale_stock_deposit_mgmt: New module to manage deposit. TT25269

---
+++ sale_stock_deposit_mgmt/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale stock deposit management",
+    "summary": "Allow to sale and delivery products in deposit",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Stock",
+    "website": "https://gitlab.tecnativa.com/Tecnativa/marmenorda-odoo",
+    "author": "Tecnativa",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_stock",
+        "stock_owner_restriction",
+    ],
+    "data": [
+        "data/sale_stock_deposit_mgmt_data.xml",
+        "views/res_partner_views.xml",
+        "views/sale_stock_deposit_mgmt_menu.xml",
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ sale_stock_deposit_mgmt/models/__init__.py
@@ -0,0 +1,4 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import res_partner
+from . import stock_quant
+from . import stock_picking
